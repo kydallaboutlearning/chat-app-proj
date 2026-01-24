d6eb86d58ad07aa941a1d49ac1bf89f03a2b63f5
Daniel <nifemiolaniyi4@gmail.com>
2026-01-24 07:45:59 +0100

    Update chatStore.ts

--- frontend/src/store/chatStore.ts
+++ frontend/src/store/chatStore.ts
@@ -41,7 +41,6 @@ export const useChatStore = create<ChatState>((set, get) => ({
     try {
       const res = await api.get('/api/users')
       if (res.data.success) {
-        console.log(res.data.users)
         set({ users: res.data.users })
       }
     } catch (error: any) {
